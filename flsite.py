4916d105ba4020c377762b157e1fc010927b2fd3
itaweb <admod32@gmail.com>
2022-09-11 21:39:28 +0300

    Create template page About

--- flsite.py
+++ flsite.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+menu = ["Установка", "Первое приложение", "Обратная связь"]
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", menu=menu)
 
 
 @app.route("/about")
 def about():
-    return "<h1>Про Flask!</h1>"
+    return render_template("about.html", title="О сайте", menu=menu)
 
 
 if __name__ == "__main__":
