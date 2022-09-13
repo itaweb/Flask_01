5fd1d63733b7e821723a79e5ec142345df022b54
itaweb <admod32@gmail.com>
2022-09-13 22:00:45 +0300

    Create pages with username

--- flsite.py
+++ flsite.py
@@ -6,15 +6,23 @@ menu = ["Установка", "Первое приложение", "Обратн
 
 @app.route("/")
 def index():
-    print( url_for("index"))
+    print(url_for("index"))
     return render_template("index.html", menu=menu)
 
 
 @app.route("/about")
 def about():
-    print( url_for("about"))
+    print(url_for("about"))
     return render_template("about.html", title="О сайте", menu=menu)
 
+
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Пользователь: {username}"
+
+
+# with app.test_request_context():
+#     print(url_for("about"))
 
 if __name__ == "__main__":
     app.run(debug=True)
