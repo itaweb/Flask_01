25555b0df1c2159cac9b07fcbe8e1e0da37dae2d
itaweb <admod32@gmail.com>
2022-09-13 21:44:21 +0300

    Add function url_for

--- flsite.py
+++ flsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 menu = ["Установка", "Первое приложение", "Обратная связь"]
@@ -6,11 +6,13 @@ menu = ["Установка", "Первое приложение", "Обратн
 
 @app.route("/")
 def index():
+    print( url_for("index"))
     return render_template("index.html", menu=menu)
 
 
 @app.route("/about")
 def about():
+    print( url_for("about"))
     return render_template("about.html", title="О сайте", menu=menu)
 
 
